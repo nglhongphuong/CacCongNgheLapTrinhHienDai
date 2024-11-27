f3e3d478a33737822157ed16c17577333b94a5bf
nglhongphuong <nglhongphuong@gmail.com>
2024-11-27 18:22:26 +0700

    Remove unnecessary files

--- mycourseproject/courses/perms.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import permissions
-
-
-class OwnerPerms(permissions.IsAuthenticated):
-    def has_object_permission(self, request, view, instance):
-        return super().has_permission(request, view) and request.user == instance.user
